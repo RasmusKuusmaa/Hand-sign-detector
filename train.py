11cfbcf00a988acc6b0803114a308e0564fed2e1
RasmusKuusmaa <77f315@gmail.com>
2024-10-07 19:20:15 +0300

    track hand

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv.VideoCapture(0)
-
+detector = HandDetector(maxHands=1)
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
     cv.imshow("image", img)
     cv.waitKey(1)
