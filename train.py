1f0a890bf356826bc610357c8a8ab1fd6318d0f9
RasmusKuusmaa <77f315@gmail.com>
2024-10-07 19:32:16 +0300

    added a little padding so a part of the hand no longer gets cut off

--- train.py
+++ train.py
@@ -3,13 +3,14 @@ from cvzone.HandTrackingModule import HandDetector
 
 cap = cv.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+offset = 20
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
         x,y,w,h = hand['bbox']
-        imgCrop = img[y:y+h, x:x+w]
+        imgCrop = img[y - offset:y+h + offset, x - offset:x+w+offset]
         if imgCrop.size > 0:
             cv.imshow('Cropped Image', imgCrop)
     cv.imshow("image", img)
